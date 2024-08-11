e26c96a5408ce79759913cf27625b36890cf8200
edembicki <edudembicki@gmail.com>
2024-08-11 00:32:22 -0300

    Remove and ignore dist/ dir

--- dist/account/dtos/create-account.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateAccountDto {
-    id: string;
-    initialBalance: number;
-}
--- dist/account/dtos/event.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class EventDto {
-    type: string;
-    destination?: string;
-    origin?: string;
-    amount: number;
-}
